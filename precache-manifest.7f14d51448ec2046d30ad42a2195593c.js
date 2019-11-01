3830f3e7dc9f4a5ad79266fe540f19909f3ccd84
PureDevPer <wooseok.kim1207@gmail.com>
2019-10-31 20:08:43 -0400

    Updates

---
+++ precache-manifest.7f14d51448ec2046d30ad42a2195593c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0519c3c09a37b7e6a5ad78f2c106be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5310b056454ccc3cb9b9",
+    "url": "/static/js/2.b26dfb21.chunk.js"
+  },
+  {
+    "revision": "2229cbf32473da1c402f",
+    "url": "/static/js/main.889729c4.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
